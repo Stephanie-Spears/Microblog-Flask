34aceadd436adcb3abd97aab9dc9d995892d54de
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-12-29 12:58:44 -0800

    apply momentjs

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_login import LoginManager
 from flask_openid import OpenID
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 from flask_mail import Mail
+from .momentjs import momentjs
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -13,6 +15,9 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 mail = Mail(app)
+
+app.jinja_env.globals['momentjs'] = momentjs
+
 
 
 
